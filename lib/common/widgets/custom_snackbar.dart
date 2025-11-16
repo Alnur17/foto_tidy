aa9a93d87f1ae2745f9a1f39e924dda3e326007d
alnur <mr.nur30@gmail.com>
2025-11-16 17:13:50 +0600

    subscription has been done and some modification

--- lib/common/widgets/custom_snackbar.dart
+++ lib/common/widgets/custom_snackbar.dart
@@ -1,20 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+// void kSnackBar({required String message, required Color bgColor}) {
+//   if (!Get.isSnackbarOpen) {
+//     Get.showSnackbar(
+//       GetSnackBar(
+//         backgroundColor: bgColor,
+//         message: message,
+//         maxWidth: 1170,
+//         duration: const Duration(seconds: 3),
+//         snackStyle: SnackStyle.FLOATING,
+//         margin: const EdgeInsets.all(10),
+//         borderRadius: 5,
+//         isDismissible: true,
+//         dismissDirection: DismissDirection.horizontal,
+//       ),
+//     );
+//   }
+// }
+
+
 void kSnackBar({required String message, required Color bgColor}) {
-  if (!Get.isSnackbarOpen) {
-    Get.showSnackbar(
-      GetSnackBar(
-        backgroundColor: bgColor,
-        message: message,
-        maxWidth: 1170,
-        duration: const Duration(seconds: 3),
-        snackStyle: SnackStyle.FLOATING,
-        margin: const EdgeInsets.all(10),
-        borderRadius: 5,
-        isDismissible: true,
-        dismissDirection: DismissDirection.horizontal,
-      ),
-    );
-  }
+  if (Get.isSnackbarOpen) Get.closeCurrentSnackbar();
+  Get.snackbar(
+    '',
+    message,
+    backgroundColor: bgColor,
+    colorText: Colors.white,
+    snackPosition: SnackPosition.BOTTOM,
+    duration: const Duration(seconds: 2),
+    margin: const EdgeInsets.all(12),
+    borderRadius: 8,
+  );
 }
